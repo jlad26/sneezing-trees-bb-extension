f2187c585cab445f9bff4bfd23dd541e8200cfa2
Jon Anwyl <jon.anwyl@gmail.com>
2020-12-14 08:55:48 +0000

    Add alignment and button to hero

--- public/bb-modules/modules/st-bb-hero/includes/frontend.php
+++ public/bb-modules/modules/st-bb-hero/includes/frontend.php
@@ -3,9 +3,29 @@
  * Hero module template.
  * @since      1.0.0
  */
+
+// Align the block of content.
+$content_align_class = '';
+if ( 'center' == $mod_params['content_align'] ) {
+    $content_align_class = ' offset-md-3';
+} elseif ( 'right' == $mod_params['content_align'] ) {
+    $content_align_class = ' offset-md-6';
+}
+
+// Align text within the block of content.
+$text_align_class = '';
+if ( 'center' == $mod_params['text_align'] ) {
+    $text_align_class = ' st-bb-text-align-center';
+} elseif ( 'right' == $mod_params['text_align'] ) {
+    $text_align_class = ' st-bb-text-align-right';
+}
+
+// Set button classes.
+$button_classes = apply_filters( 'st_bb_hero_button_classes', 'st-bb-btn', $module );
+
 ?>
 <div class="st-bb-hero-content row">
-    <div class="col-md-6">
+    <div class="col-md-6<?php echo $content_align_class . $text_align_class; ?>">
         <?php if ( isset( $mod_params['title'] ) ) : ?>
         <h1 class="st-bb-hero-title">
             <?php echo esc_html( $mod_params['title'] ); ?>
@@ -18,10 +38,10 @@
         </h2>
         <?php endif; ?>
 
-        <?php if ( isset( $mod_params['button'] ) ) : ?>
-        <a type="button" class="st-bb-button<?php esc_attr_e( $mod_params['button_classes'] ); ?>" href="<?php echo esc_url( $mod_params['button_url'] ); ?>">
-            <?php echo esc_html( $mod_params_content['button_text'] ); ?>
-        </button>
+        <?php if ( $mod_params['button_url'] && $mod_params['button_text'] ) : ?>
+        <a class="<?php esc_attr_e( $button_classes ); ?>" href="<?php echo esc_url( $mod_params['button_url'] ); ?>">
+            <?php echo esc_html( $mod_params['button_text'] ); ?>
+        </a>
         <?php endif; ?>
     </div>
 </div>
--- public/bb-modules/modules/st-bb-hero/st-bb-hero.php
+++ public/bb-modules/modules/st-bb-hero/st-bb-hero.php
@@ -48,11 +48,11 @@ class ST_BB_Hero_Module extends ST_BB_Module {
             'module'    => array(
                 'sections'		=>  array(
                     'text'		=>  array(
-                        'title'         =>  'Text',
+                        'title'         =>  __( 'Text', ST_BB_TD ),
                         'fields'        =>  array(
                             'title'		=>  array(
                                 'type'          =>  'text',
-                                'label'         =>  'Heading',
+                                'label'         =>  __( 'Heading', ST_BB_TD ),
                                 'preview'       =>   array(
                                     'type'          =>    'text',
                                     'selector'      =>    '.st-bb-hero-title',
@@ -61,12 +61,62 @@ class ST_BB_Hero_Module extends ST_BB_Module {
                             ),
                             'subtitle'		=>  array(
                                 'type'          =>  'text',
-                                'label'         =>  'Sub-heading',
+                                'label'         =>  __( 'Sub-heading', ST_BB_TD ),
                                 'preview'       =>   array(
                                     'type'          =>    'text',
                                     'selector'      =>    '.st-bb-hero-subtitle',
                                 ),
                                 'sanitize'		=>	'sanitize_text_field',
+                            ),
+                        ),
+                    ),
+                    'button'	=> array(
+						'title'		=>	'Button',
+						'fields'	=>	array(
+							'button_text'	=> array(
+								'type'          => 'text',
+								'label'         => 'Text',
+								'default'       => '',
+								'preview'         => array(
+									'type'            => 'text',
+									'selector'        => '.st-bb-btn',
+								),
+								'sanitize'		=>	'sanitize_text_field',
+							),
+							'button_url'	=> array(
+								'type'          => 'text',
+								'label'         => 'Link',
+								'default'       => '',
+								'sanitize'		=>	'esc_url_raw',
+							),
+						),
+					),
+                    'h_align'		=>  array(
+                        'title'         =>  __( 'Horizontal alignment', ST_BB_TD ),
+                        'fields'        =>  array(
+                            'content_align'		=>  array(
+                                'type'          =>  'select',
+                                'label'         =>  __( 'Content block', ST_BB_TD ),
+                                'default'       =>  'left',
+                                'options'	=> array(
+                                    'left'      =>  __( 'Left', ST_BB_TD ),
+                                    'center'    =>	__( 'Center', ST_BB_TD ),
+                                    'right'     =>	__( 'Right', ST_BB_TD ),
+                                ),
+                                'sanitize'		=>	'sanitize_text_field',
+                                'help'          =>  __( 'Determines where the block of content is placed.', ST_BB_TD ),
+                            ),
+                            'text_align'		=>  array(
+                                'type'          =>  'select',
+                                'label'         =>  __( 'Text', ST_BB_TD ),
+                                'default'       =>  'left',
+                                'options'	=> array(
+                                    'left'      =>  __( 'Left', ST_BB_TD ),
+                                    'center'    =>	__( 'Center', ST_BB_TD ),
+                                    'right'     =>	__( 'Right', ST_BB_TD ),
+                                ),
+                                'sanitize'		=>	'sanitize_text_field',
+                                'help'          =>  __( 'Determines how text is aligned within the block of content.', ST_BB_TD ),
                             ),
                         ),
                     ),
