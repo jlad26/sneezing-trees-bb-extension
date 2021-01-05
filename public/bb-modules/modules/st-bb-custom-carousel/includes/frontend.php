3442f9014fd3fe2d193ff0b7de54541ab10988d6
Jon Anwyl <jon.anwyl@gmail.com>
2021-01-05 16:05:01 +0000

    Add custom carousel module

---
+++ public/bb-modules/modules/st-bb-custom-carousel/includes/frontend.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Custom Carousel module template.
+ * @since      1.0.0
+ */
+
+$mod_params['slides'] = apply_filters( 'st-bb-custom-carousel-slides-' . $mod_params['custom_carousel_id'], array() );
+
+?>
+<div class="row">
+    <div class="col-lg-10 offset-lg-1">
+        <div class="swiper-container">
+            <div class="swiper-wrapper">
+        <?php
+            if ( is_array( $mod_params['slides'] ) ) {
+                foreach ( $mod_params['slides'] as $slide ) {
+        ?>
+                <div class="swiper-slide">
+                    <?php echo $slide; ?>
+                </div>
+        <?php
+                }
+            }
+        ?>
+            </div>
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+        </div>
+    </div>
+</div>
---
+++ public/bb-modules/modules/st-bb-custom-carousel/st-bb-custom-carousel.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Defines the Custom Carousel module.
+ *
+ * @since      1.0.0
+ *
+ * @package    ST_BB
+ * @subpackage ST_BB/public
+ */
+
+/**
+ * Defines the Custom Carousel module.
+ *
+ * Defines the Custom Carousel module class and settings.
+ *
+ * @package    ST_BB
+ * @subpackage ST_BB/public
+ */
+class ST_BB_Custom_Carousel_Module extends ST_BB_Module {
+    
+    /**
+	 * Constructor.
+	 *
+	 * @since    1.0.0
+	 */
+    public function __construct() {
+        parent::__construct( array(
+            'name'              =>  __( 'Custom Carousel', ST_BB_TD ),
+            'description'       =>  __( 'Custom Carousel', ST_BB_TD ),
+            'icon'              =>  'format-gallery.svg',
+            'partial_refresh'   =>  true,
+            'config'            =>  array(
+                'acf_version'       =>  true,
+                'js'                =>  array(
+                    array(
+                        'handle'    =>  'bb-swiper',
+                        'src'       =>  'https://unpkg.com/swiper/swiper-bundle.min.js',
+                    ),
+                ),
+                'css'                =>  array(
+                    array(
+                        'handle'    =>  'bb-swiper',
+                        'src'       =>  'https://unpkg.com/swiper/swiper-bundle.css'
+                    ),
+                ),
+
+            )
+        ) );
+
+    }
+
+    /**
+	 * Get the settings to be applied on module registration.
+	 *
+	 * @since    1.0.0
+     * 
+     * @return  array
+	 */
+    protected static function get_module_config() {
+        return array(
+            'module'    =>  array(
+                'title' =>  __( 'Contents', ST_BB_TD ),
+                'sections'		=>  array(
+                    'carousel_id'     =>  array(
+                        'title'         =>  __( 'Carousel ID', ST_BB_TD ),
+                        'fields'        =>  array(
+                            'custom_carousel_id'  => array(
+                                'type'          =>  'text',
+                                'label'         =>  __( 'Carousel ID', ST_BB_TD ),
+                                'help'          =>  __( 'Give the carousel a unique ID. Slides content can then be added using the filter \'st-bb-custom-carousel-slides-{$your_id}\'.', ST_BB_TD ),
+                                'sanitize'		=>	'sanitize_text_field',
+                            ),
+                        ),
+                    ),
+                ),
+            ),
+        );
+    }
+    
+}
+?>
