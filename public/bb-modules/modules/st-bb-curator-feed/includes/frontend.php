9fe6f213582e912db75ee6a084a193e639649a62
Jon Anwyl <jon.anwyl@gmail.com>
2021-01-08 16:49:25 +0000

    Amend curator styling

--- public/bb-modules/modules/st-bb-curator-feed/includes/frontend.php
+++ public/bb-modules/modules/st-bb-curator-feed/includes/frontend.php
@@ -6,7 +6,7 @@
 ?>
 <div class="container">
     <div class="row">
-        <div class="col-md-10 offset-md-1 col-lg-6 offset-lg-3">
+        <div class="col-md-10 offset-md-1 col-lg-8 offset-lg-2">
             <div id="curator_feed_<?php echo $mod_params['curator_feed_id']; ?>">
                 <a href="https://curator.io" target="_blank" class="crt-logo crt-tag">Powered by Curator.io</a>
             </div>
