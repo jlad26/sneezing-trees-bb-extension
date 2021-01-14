bee773a6b935c28274719349aab4dc9604868406
Jon Anwyl <jon.anwyl@gmail.com>
2021-01-14 12:35:28 +0000

    Add style so that content stretches to full width

--- public/bb-modules/modules/st-bb-two-col-free-edit/includes/frontend.php
+++ public/bb-modules/modules/st-bb-two-col-free-edit/includes/frontend.php
@@ -8,21 +8,25 @@ global $wp_embed;
 ?>
 <div class="row">
     <div class="st-bb-rich-text-1 col-lg-6 col-xl-5 offset-xl-1 st-bb-text-col-1">
-        <?php
-        if ( isset( $mod_params['free_content_1'] ) ) {
-            echo wp_kses_post( wpautop( $wp_embed->autoembed( $mod_params['free_content_1'] ) ) );
-        }
-        $field_prefix = 'col_1';
-        include ST_BB_DIR . 'public/partials/button.php';
-        ?>
+        <div>
+            <?php
+            if ( isset( $mod_params['free_content_1'] ) ) {
+                echo wp_kses_post( wpautop( $wp_embed->autoembed( $mod_params['free_content_1'] ) ) );
+            }
+            $field_prefix = 'col_1';
+            include ST_BB_DIR . 'public/partials/button.php';
+            ?>
+        </div>
     </div>
     <div class="st-bb-rich-text-2 col-lg-6 col-xl-5 st-bb-text-col-2">
-        <?php
-        if ( isset( $mod_params['free_content_2'] ) ) {
-            echo wp_kses_post( wpautop( $wp_embed->autoembed( $mod_params['free_content_2'] ) ) );
-        }
-        $field_prefix = 'col_2';
-        include ST_BB_DIR . 'public/partials/button.php';
-        ?>
+        <div>
+            <?php
+            if ( isset( $mod_params['free_content_2'] ) ) {
+                echo wp_kses_post( wpautop( $wp_embed->autoembed( $mod_params['free_content_2'] ) ) );
+            }
+            $field_prefix = 'col_2';
+            include ST_BB_DIR . 'public/partials/button.php';
+            ?>
+        </div>
     </div>
 </div>
