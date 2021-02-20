b43132f55097eb0db0f39700b3ed96195a754a2d
Jon Anwyl <jon.anwyl@gmail.com>
2021-02-20 11:08:23 +0000

    Change breakpoint for single col

--- public/bb-modules/modules/st-bb-hero/includes/frontend.php
+++ public/bb-modules/modules/st-bb-hero/includes/frontend.php
@@ -12,9 +12,9 @@ if ( 'left' == $mod_params['content_align'] ) {
         $content_align_class = ' col-xl-5 offset-xl-1';
     }
 } elseif ( 'center' == $mod_params['content_align'] ) {
-    $content_align_class = ' offset-md-3';
+    $content_align_class = ' offset-lg-3';
 } elseif ( 'right' == $mod_params['content_align'] ) {
-    $content_align_class = ' offset-md-6';
+    $content_align_class = ' offset-lg-6';
     if ( $desktop_indent ) {
         $content_align_class .= ' col-xl-5';
     }
@@ -29,7 +29,7 @@ if ( isset( $mod_params['free_content'] ) ) {
 
 ?>
 <div class="st-bb-hero-content row">
-    <div class="col-md-6 st-bb-rich-text<?php echo $content_align_class; ?>">
+    <div class="col-lg-6 st-bb-rich-text<?php echo $content_align_class; ?>">
         <?php echo $content; ?>
         <?php include ST_BB_DIR . 'public/partials/button.php'; ?>
     </div>
